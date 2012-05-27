aa4e73438b6b77629d4b4bb14f984e3a07d438cb
ndeverge <ndeverge@gmail.com>
2012-05-27 15:37:42 +0200

    Add pictures

--- app/mock/PicturesListMock.java
+++ app/mock/PicturesListMock.java
@@ -13,55 +13,56 @@ import play.Logger;
 
 public class PicturesListMock {
 
-	public static Map<String, Picture> pictures = new HashMap<String, Picture>();
+    public static Map<String, Picture> pictures = new HashMap<String, Picture>();
 
-	static {
-		// load sample images
-		createSamplePicture("/mock/fnac.jpg", "67890", "image/jpeg");
-		createSamplePicture("/mock/caissiere.jpg", "13579", "image/jpeg");
-		createSamplePicture("/mock/77777.jpg", "77777", "image/jpeg");
-		createSamplePicture("/mock/99999.jpg", "99999", "image/jpeg");
-		createSamplePicture("/mock/100001.jpg", "100001", "image/jpeg");
-		createSamplePicture("/mock/100003.jpg", "100003", "image/jpeg");
-		createSamplePicture("/mock/100007.jpg", "100007", "image/jpeg");
-		createSamplePicture("/mock/100019.jpg", "100019", "image/jpeg");
-		createSamplePicture("/mock/100023.jpg", "100023", "image/jpeg");
-		createSamplePicture("/mock/100024.jpg", "100024", "image/jpeg");
+    static {
+        // load sample images
+        createSamplePicture("/mock/fnac.jpg", "67890", "image/jpeg");
+        createSamplePicture("/mock/caissiere.jpg", "13579", "image/jpeg");
+        createSamplePicture("/mock/77777.jpg", "77777", "image/jpeg");
+        createSamplePicture("/mock/99999.jpg", "99999", "image/jpeg");
+        createSamplePicture("/mock/100001.jpg", "100001", "image/jpeg");
+        createSamplePicture("/mock/100003.jpg", "100003", "image/jpeg");
+        createSamplePicture("/mock/100007.jpg", "100007", "image/jpeg");
+        createSamplePicture("/mock/100019.jpg", "100019", "image/jpeg");
+        createSamplePicture("/mock/100023.jpg", "100023", "image/jpeg");
+        createSamplePicture("/mock/100024.jpg", "100024", "image/jpeg");
+        createSamplePicture("/mock/big-mac.jpg", "11111", "image/jpeg");
+        createSamplePicture("/mock/midica.jpg", "98765", "image/jpeg");
+        createSamplePicture("/mock/celiosport.jpg", "33333", "image/jpeg");
 
-	}
+    }
 
-	private static Picture createSamplePicture(String path, String id,
-			String contentType) {
-		InputStream picture2AsStream = PicturesListMock.class
-				.getResourceAsStream(path);
+    private static Picture createSamplePicture(final String path, final String id, final String contentType) {
+        InputStream picture2AsStream = PicturesListMock.class.getResourceAsStream(path);
 
-		if (picture2AsStream != null) {
-			try {
-				Logger.debug("Loading mock image");
-				byte[] bytes = IOUtils.toByteArray(picture2AsStream);
+        if (picture2AsStream != null) {
+            try {
+                Logger.debug("Loading mock image");
+                byte[] bytes = IOUtils.toByteArray(picture2AsStream);
 
-				return addPicture(id, bytes, contentType);
+                return addPicture(id, bytes, contentType);
 
-			} catch (IOException e) {
-				Logger.error("Error loading an image", e);
-			}
-		} else {
-			Logger.error("Image not found");
-		}
-		return null;
-	}
+            } catch (IOException e) {
+                Logger.error("Error loading an image", e);
+            }
+        } else {
+            Logger.error("Image not found");
+        }
+        return null;
+    }
 
-	public static Picture addPicture(String id, byte[] bytes, String contentType) {
-		Picture picture = new Picture(id, bytes, contentType);
+    public static Picture addPicture(final String id, final byte[] bytes, final String contentType) {
+        Picture picture = new Picture(id, bytes, contentType);
 
-		pictures.put(id, picture);
+        pictures.put(id, picture);
 
-		return picture;
-	}
+        return picture;
+    }
 
-	public static Picture findPicture(String id) {
+    public static Picture findPicture(final String id) {
 
-		return pictures.get(id);
-	}
+        return pictures.get(id);
+    }
 
 }
